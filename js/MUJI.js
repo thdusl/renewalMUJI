762f4330d7236f9bc9d4ebaa4008a191cefb7329
thdusl <155565613+thdusl@users.noreply.github.com>
2024-03-25 09:37:25 +0900

    수정 2트

--- js/MUJI.js
+++ js/MUJI.js
@@ -1,13 +1,17 @@
 $(function(){
+        
     //접속한 브라우저의 가로길이 가져오기
     let deviceWidth=$(window).width();
 
     //태블릿과 모바일에서 메뉴 버튼을 클릭하면 내비게이션이 오른쪽에서 나타남
     $(".menu_icon").click(function(){
+        let menuHeight = $(".menu").height();
+        $(".wrap").css({"height":menuHeight,"overflow":"hidden"});
         $(".menu").animate({ 'right':0 });
     });
     //태블릿과 모바일에서 닫기 버튼을 클릭하면 내비게이션이 오른쪽으로 사라짐
     $(".close_btn").click(function(){
+        $(".wrap").css({"height":"auto","overflow":"visible"});
         $(".menu").animate({ 'right':'-100vw' });
     });
 
@@ -36,6 +40,7 @@ $(function(){
         });
     //접속한 기기의 가로길이가 1400px이상이면 주메뉴에 마우스 오버했을 때 서브메뉴와 서브메뉴 배경이 나타나고, 마우스 아웃했을 때 서브메뉴와 서브메뉴 배경이 사라짐
     } else {
+        //pc버전 내비게이션
         $("nav > ul > li > a").mouseenter(function(){
             $(".sub").stop().slideDown();
             $(".sub_bg").stop().slideDown();
@@ -44,7 +49,16 @@ $(function(){
             $(".sub").stop().slideUp();
             $(".sub_bg").stop().slideUp();
         });
+        //pc버전 스크롤하면 화면 단위로 이동함
+        $(".fullpage").fullpage({
+            navigation:true,
+            //내가 추가 (근데 스와이퍼가 있는 상품란이 안됨)
+            anchors:["","","anchor3","anchor4","anchor5","anchor6","anchor7",],
+            navigationTooltips:["","기획전","신상품&인기상품","봄 아이템","세일","매일 매거진","계절의 추천"],
+            showAciveTooltip:true
+        });
     }
+
 
     //글로벌 사이트(family site)
     $(".family_title a").click(function(e){
@@ -68,12 +82,16 @@ $(function(){
           clickable: true,
         },
         breakpoints: {
+            500: {
+                slidesPerView: 3,  //브라우저가 500보다 클 때 3개
+                spaceBetween: 30,
+            },
             1024: {
                 slidesPerView: 3,  //브라우저가 1024보다 클 때 3개
                 spaceBetween: 30,
             },
             1400: {
-                slidesPerView: 4,  //브라우저가 1400보다 클 때 4개
+                slidesPerView: 5,  //브라우저가 1400보다 클 때 4개
                 spaceBetween: 30,
             },
         },
